e039faba8f2c33a43a9bebf0bb6e26992215bd04
Paul Hobson <pmhobson@gmail.com>
2021-04-03 08:14:52 -0700

    list validator

--- iowag/validate.py
+++ iowag/validate.py
@@ -1,6 +1,20 @@
+import numpy
+
+
 def single_geoms(geoms):
     assert all(len(g) == 1 for g in geoms)
 
 
 def same_crs(crs1, crs2):
     assert crs1 == crs2
+
+
+def at_least_empty_list(value):
+    if isinstance(value, numpy.ndarray):
+        value = value.tolist()
+    elif numpy.isscalar(value) and value != "":
+        value = [value]
+    elif not value:
+        value = []
+
+    return value
